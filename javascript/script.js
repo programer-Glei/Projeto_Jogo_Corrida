9a0989a56d166c97e3cb96d9c6c1cb6d5ad25825
Gleibson Pereira <gleibson.santos05@gmail.com>
2021-04-24 21:09:11 -0300

    proximo comit

--- javascript/script.js
+++ javascript/script.js
@@ -5,6 +5,7 @@ var soma = randomize(10,20);
 var botao = document.querySelector("#botao");
 var btn_comeca = document.querySelector("#btn-ini")
 var modal = document.querySelector('.modal-overaly')
+var carro_selecionado;
 
 
 var carro1 = [document.querySelector("#carro1"),document.querySelector("#carro2"),document.querySelector("#carro3"),document.querySelector("#carro4")];
@@ -22,6 +23,8 @@ botao.addEventListener("click", () =>{
 
 btn_comeca.addEventListener("click", () =>{
     modal.classList.remove('visivel')
+    carro_selecionado = document.querySelector('input[name="carro"]:checked').value;
+    console.log(carro_selecionado)
     game = setInterval(moveImage, 100);
 })
 
@@ -47,13 +50,18 @@ function moveImage(){
         carro1[i].style.left = (parseInt(carro1[i].style.left) + carroCorrida[i].velocidade) + 'px';
         console.log(parseInt(carro1[1].style.left))
         if( parseInt(carro1[i].style.left) >= 1380){
-            var vencedor = carroCorrida[i].nome
-            fim(vencedor);
+            var vencedor = i
+            var nome = carroCorrida[i].nome
+            fim(vencedor,nome);
         }
     }
 }
 
-function fim(vencedor){
+function fim(vencedor,nome){
     clearInterval(game);
-    alert("O "+vencedor+" venceu")
+    if(vencedor == carro_selecionado){
+        alert("O "+nome+" venceu, você acertou viséravi")
+    }else{
+        alert("O "+vencedor+" venceu, tenta outra vez otário")
+    }
 }
